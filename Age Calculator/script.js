b4b46361bd62d3db8af4caff3469e95efde2a580
nrb007 <nrb121903@gmail.com>
2024-10-01 20:38:28 -0400

    dfsdf

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -1,6 +1,7 @@
 
 let userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("result");
 
 function calculateAge(){
     let birthDate = new Date(userInput.value);
@@ -14,5 +15,37 @@ function calculateAge(){
     let d2 = today.getDate();
     let m2 = today.getMonth() + 1;
     let y2 = today.getFullYear();
+
+    let d3, m3, y3;
+
+    // Current Year - Selected Year
+    y3 = y2 - y1;
+
+    // Month calc
+    if(m2 >= m1){ 
+        m3 = m2 - m1;
+    }
+    else{
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
     
+    // Day calc
+    if(d2 >= d1){ 
+        d3 = d2 - d1;
+    }
+    else{
+        m3--;
+        d3 = getDaysInMonth(y1, m1) + d2 - d1;
+    }
+    if(m3 < 0){
+        m3 = 11;
+        y3--;
+    }
+    
+    result.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`;
+}
+
+function getDaysInMonth(year, month){
+    return new Date(year, month, 0).getDate();
 }
